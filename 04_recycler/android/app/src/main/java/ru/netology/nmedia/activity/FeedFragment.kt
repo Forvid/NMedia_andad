9b36d683229d4c6e334c9ce93fe4aed07fb300be
Forvid <vitodanlanc@gmail.com>
2025-06-25 19:34:05 +0600

    показывать SwipeRefreshLayout только на REFRESH

--- 04_recycler/android/app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
+++ 04_recycler/android/app/src/main/java/ru/netology/nmedia/activity/FeedFragment.kt
@@ -1,3 +1,30 @@
+package ru.netology.nmedia.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.FeedAdapter
+import ru.netology.nmedia.adapter.PagingLoadStateAdapter
+import ru.netology.nmedia.databinding.FragmentFeedBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
 @AndroidEntryPoint
 class FeedFragment : Fragment() {
 
@@ -72,3 +99,4 @@ class FeedFragment : Fragment() {
         return binding.root
     }
 }
+
